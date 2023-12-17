3b3b0b3a3df530c3f22306b94144e89cb40a65ff
Mahmoud Abu Youssef <mahmoudabuyoussef20@gmail.com>
2023-12-17 11:46:26 +0200

    Start Project && Create Navbar && Hero Section

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+let barsIcon = document.getElementById("bars-icon");
+let navLinks = document.getElementById("nav-links");
+let btnSearch = document.getElementById("btn-seacrh");
+
+function handelClick() {
+  navLinks.classList.add("active");
+}
+
+function seachClick() {
+  btnSearch.classList.add("active");
+}
